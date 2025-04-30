fbb69a07049465f7cdc6ae27b61ed2039a68cde8
Ludoviko451 <luisangelquirosflorez@gmail.com>
2025-04-29 21:50:43 -0500

    feat: add validation handler to return error messages in REST requests

---
+++ src/main/java/com/laqf/pulsevote/config/ValidationHandler.java
@@ -0,0 +1,23 @@
+package com.laqf.pulsevote.config;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import java.util.List;
+
+@ControllerAdvice
+public class ValidationHandler {
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<List<String>> handleException(WebExchangeBindException e) {
+        var errors = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
